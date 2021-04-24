ad83379aae6afb2019cbe79b4b67b45a6b022dbd
omsatam <62635289+omsatam@users.noreply.github.com>
2021-04-24 16:24:18 +0530

    Update Video.js

--- src/Video.js
+++ src/Video.js
@@ -7,16 +7,7 @@ import tiktok from "./tiktok.png";
 function Video({ url, chanel, description, song, likes, messages, shares }) {
   const [play, setPlay] = useState(false);
   const videoRef = useRef(null);
-  //1. description :Flying low through palm trees and down towards a tropical beach. Lombok, Indonesia.
-  //title: Flying Through Palm Trees Along Beach
-  //chanel: videvo
-  // ocean: https://cdn.videvo.net/videvo_files/video/free/2017-08/small_watermarked/170724_15_Setangibeach_preview.webm
-
-  //2. Sunset At Venice Beach LA
-  //Sun setting on the ocean at Venice Beach, LA.
-  // sunset: https://cdn.videvo.net/videvo_files/video/free/2019-01/small_watermarked/181015_13_Venice%20Beach%20Drone_25_preview.webm
-
-  //3.
+ 
   const onVideoPress = () => {
     if (play) {
       videoRef.current.pause();
